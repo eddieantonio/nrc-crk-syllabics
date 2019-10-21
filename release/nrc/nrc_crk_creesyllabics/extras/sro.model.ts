6c427328ef6f25ce2d405041b7d4140817190334
Eddie Antonio Santos <easantos@ualberta.ca>
2019-10-21 14:26:11 -0600

    Simpler searchterm to key function.

--- release/nrc/nrc_crk_creesyllabics/extras/sro.model.ts
+++ release/nrc/nrc_crk_creesyllabics/extras/sro.model.ts
@@ -1,5 +1,10 @@
 const model = {
-    format: 'trie-1.0',
-    sources: ['wordlist.tsv'],
+  format: 'trie-1.0',
+  sources: ['wordlist.tsv'],
+  searchTermToKey: function(term: string): string {
+    return term.replace(/[êîôâ]|-/g, (m: string) => ({
+      ê: 'e', î: 'i', ô: 'o', â: 'a', '-': ''
+    }[m]));
+  },
 };
 export default model;
