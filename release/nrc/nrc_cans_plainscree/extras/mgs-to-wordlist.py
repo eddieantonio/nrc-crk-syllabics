5a40c77fbabf47ff01205ace554c9d571d4f73d2
Eddie Antonio Santos <easantos@ualberta.ca>
2019-10-21 13:15:08 -0600

    Sure.

--- release/nrc/nrc_cans_plainscree/extras/mgs-to-wordlist.py
+++ release/nrc/nrc_cans_plainscree/extras/mgs-to-wordlist.py
@@ -6,41 +6,46 @@ from collections import Counter
 from unicodedata import normalize
 from functools import partial
 
-# These are mostly mistakes in tagging:
-BANNED_LEMMATA = ["IC", "V", "N"]
-DISALLOWED_LIST = [
+VALID_POS = [
+    "V",
+    "N",
+    "Ipc",
     "PUNCT",
     "CLB",
-    "^excl",
-    "Num",
+    "Eng",
     "Lat",
-    "Err/Frag",
+    "Pron",
+    "Num",
     "?",
-    "Art",
-    "Err/Orth",
-    "Code",
-    "Frag",
+    "Err/Frag",
+    "Verb",
+    "Noun",
+    "Adj",
     "Morph",
-    "Lemma",
-    # This one is a mistake: ignore
-    "wêpinam",
-    # This is also a mistake:
-    "AI",
-    # Mistake:
-    "PV/aya",
-    None,
+    "Ord",
+    "Prop",
+    "Code",
 ]
-# Cnj is a typo, but whatever
-APPROVED_POS = ["V", "N", "Ipc", "Pron", "Cnj"]
 
+ALLOWED_POS = [
+    "V",
+    "N",
+    "Ipc",
+    "Pron",
+]
 
 class Analysis:
     def __init__(self, text):
-        full_analysis = [tag for tag in text.split("+") if tag.strip()]
-        lemma_pos = get_lemma_pos(full_analysis)
-        self.prefixes = full_analysis[:lemma_pos]
-        self.lemma = full_analysis[lemma_pos]
-        self.suffixes = full_analysis[lemma_pos + 1 :]
+        if text.startswith("+") and "+" not in text[1:]:
+            self.prefixes = ()
+            self.suffixes = (text.lstrip("+"),)
+            self.lemma = ""
+        else:
+            full_analysis = [tag for tag in text.split("+") if tag.strip()]
+            lemma_pos = get_lemma_pos(full_analysis)
+            self.prefixes = full_analysis[:lemma_pos]
+            self.lemma = full_analysis[lemma_pos]
+            self.suffixes = full_analysis[lemma_pos + 1 :]
 
     @property
     def pos(self):
@@ -61,6 +66,10 @@ class NoPosError(Exception):
     pass
 
 
+class InvalidAnalysisError(Exception):
+    pass
+
+
 def get_lemma_pos(analysis):
     for pos, tag in enumerate(analysis):
         if tag.startswith(("PV/", "Rdpl", "*Rdpl", "*PV/")):
@@ -74,37 +83,54 @@ nfc = partial(normalize, "NFC")
 
 word_count = Counter()
 
-with open("ahenakew_wolfart_MGS_tab-sep-anls_freq-sorted.txt", encoding="UTF-8") as mgs:
-    for line in mgs:
-        line = line.strip()
+
+def add_from_line(line):
+    line = line.strip()
+
+    if not line:
+        return
+
+    try:
+        count_string, wordform, *raw_analyses = line.split()
+    except ValueError:
+        raise InvalidAnalysisError
+
+    good_analyses = []
+    for text in raw_analyses:
+        if "Frag" in text or "Err/" in text:
+            continue
 
         try:
-            count_string, wordform, *raw_analyses = line.split()
-        except ValueError:
-            # Malformed line... :/
+            analysis = Analysis(text)
+        except NoPosError:
+            raise InvalidAnalysisError
+
+        if analysis.pos not in VALID_POS:
+            raise InvalidAnalysisError
+
+        if analysis.pos not in ALLOWED_POS:
             continue
 
-        good_analyses = []
-        for text in raw_analyses:
-            try:
-                analysis = Analysis(text)
-            except NoPosError:
-                # A mistake, probably
-                continue
-
-            if analysis.lemma in BANNED_LEMMATA:
-                continue
-            if analysis.pos in DISALLOWED_LIST:
-                continue
-            if analysis.is_english:
-                continue
-            assert analysis.pos in APPROVED_POS, f"{line!r}: {analysis!r}"
-            good_analyses.append(analysis)
-
-        if not good_analyses:
+        if 'Prop' in analysis.suffixes:
             continue
 
-        word_count[nfc(wordform)] += int(count_string)
+        if analysis.is_english:
+            continue
+
+        good_analyses.append(analysis)
+
+    if not good_analyses:
+        return
+
+    word_count[nfc(wordform)] += int(count_string)
+
+
+with open("ahenakew_wolfart_MGS_tab-sep-anls_freq-sorted.txt", encoding="UTF-8") as mgs:
+    for lineno, line in enumerate(mgs):
+        try:
+            add_from_line(line)
+        except InvalidAnalysisError:
+            print("Invalid line:", line.strip(), file=sys.stderr)
 
 
 print("#word\tcount")
