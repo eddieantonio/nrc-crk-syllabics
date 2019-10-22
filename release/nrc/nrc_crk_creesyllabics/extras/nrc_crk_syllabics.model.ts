b88295e8e57892cccc329f83bdf152d996ed1443
Eddie Antonio Santos <easantos@ualberta.ca>
2019-10-22 08:52:41 -0600

    It kind of works :/

--- release/nrc/nrc_crk_creesyllabics/extras/nrc_crk_syllabics.model.ts
+++ release/nrc/nrc_crk_creesyllabics/extras/nrc_crk_syllabics.model.ts
@@ -2,6 +2,7 @@ import '@keymanapp/lexical-model-types';
 
 const model = {
   format: 'custom-1.0',
-  sources: ['syllabics2sro.ts', 'custom-model.ts', 'FOOTER.ts']
+  sources: ['syllabics2sro.ts', 'custom-model.ts'],
+  rootClass: 'PlainsCreeSyllabicsModel',
 };
 export default model;
