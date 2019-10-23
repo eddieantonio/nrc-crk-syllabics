45bd64ec39ead42d34fe4d4817aeefaed5002d3f
Eddie Antonio Santos <easantos@ualberta.ca>
2019-10-23 13:40:00 -0600

    Embetter the custom model implementation.

--- release/nrc/nrc_crk_syllabics/source/plains-cree-syllabics-model.ts
+++ release/nrc/nrc_crk_syllabics/source/plains-cree-syllabics-model.ts
@@ -14,27 +14,45 @@ declare function syllabics2sro(syllabics: string): string;
 declare function createTrieModel(): models.TrieModel;
 
 class PlainsCreeSyllabicsModel implements LexicalModel {
-  private _underlyingModel: models.TrieModel;
-  constructor() {
-    this._underlyingModel = createTrieModel();
-  }
+
+  /**
+   * The actual work being done by an model whose inputs are in standard Roman
+   * orthography (Latn), however it will always output in syllabics (Cans).
+   */
+  private readonly _underlyingLatnModel = createTrieModel();
 
   configure(capabilities: Capabilities): Configuration {
-    return this._underlyingModel.configure(capabilities);
+    return this._underlyingLatnModel.configure(capabilities);
   }
 
+  /**
+   * Takes the transform/context 
+   * 
+   * @param transform buffer transformation, in Cans
+   * @param context buffer context, in Cans
+   */
   predict(transform: Transform, context: Context): ProbabilityMass<Suggestion>[] {
-    // TODO: stuff
-    return this._underlyingModel.predict(transform, context);
+    // Convert input from Cans to Latn
+    let latnTransform: Transform = Object.assign({}, transform, {
+      insert: syllabics2sro(transform.insert)
+    });
+    let latnContext: Context = Object.assign({}, context, {
+      left: syllabics2sro(context.left)
+    });
+
+    return this._underlyingLatnModel.predict(latnTransform, latnContext);
   }
 
   wordbreak(context: Context): string {
-    // TODO: other stuff
-    return this._underlyingModel.wordbreak(context);
+    return this._underlyingLatnModel.wordbreak(context);
   }
 
+  /**
+   * I'm not super sure what punctuation to use for quotes, but I'm going to
+   * explicitly define these punctuation.
+   */
   punctuation: LexicalModelPunctuation = {
-    insertAfterWord: ' ',
+    insertAfterWord: ' ', // insert a simple space after words
     quotesForKeepSuggestion: {
       open: "“",
       close: "”"
