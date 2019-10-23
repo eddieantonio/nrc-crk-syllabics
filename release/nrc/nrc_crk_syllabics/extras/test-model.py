52a0ab8565afe30a60104a651b1d0218d3494dc1
Eddie Antonio Santos <easantos@ualberta.ca>
2019-10-23 14:04:30 -0600

    Reformat some Python.

--- release/nrc/nrc_crk_syllabics/extras/test-model.py
+++ release/nrc/nrc_crk_syllabics/extras/test-model.py
@@ -7,19 +7,19 @@ from pathlib import Path
 
 HERE = Path(__file__).parent
 
-MODEL_PATH = HERE / '..'  / 'build' / 'nrc.crk.syllabics.model.js'
-
-
+MODEL_PATH = HERE / ".." / "build" / "nrc.crk.syllabics.model.js"
 
 
 class TestModel(unittest.TestCase):
     def test_basic(self):
-        self.assertIn('ᐁᑿ', predict("ᐁᐠᐤ"))
+        self.assertIn("ᐁᑿ", predict("ᐁᐠᐤ"))
+
 
 def predict(text: str) -> dict:
-    raw_output = check_output(['lmlayer-cli', '-f', MODEL_PATH, '-p', text])
-    return raw_output.decode('UTF-8').split("\n")[0].split("\t")[1:]
+    raw_output = check_output(["lmlayer-cli", "-f", MODEL_PATH, "-p", text])
+    return raw_output.decode("UTF-8").split("\n")[0].split("\t")[1:]
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     assert MODEL_PATH.exists(), MODEL_PATH
     unittest.main()
