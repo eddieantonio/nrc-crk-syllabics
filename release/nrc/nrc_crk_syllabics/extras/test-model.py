9407ca784b0fbf96deddea557a7b1e3bd64ebc53
Eddie Antonio Santos <easantos@ualberta.ca>
2019-10-23 13:57:23 -0600

    Have an automated way for testing the model.

---
+++ release/nrc/nrc_crk_syllabics/extras/test-model.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import unittest
+from subprocess import check_output
+from pathlib import Path
+
+HERE = Path(__file__).parent
+
+MODEL_PATH = HERE / '..'  / 'build' / 'nrc.crk.syllabics.model.js'
+
+
+
+
+class TestModel(unittest.TestCase):
+    def test_basic(self):
+        self.assertIn('ᐁᑿ', predict("ᐁᐠᐤ"))
+
+def predict(text: str) -> dict:
+    raw_output = check_output(['lmlayer-cli', '-f', MODEL_PATH, '-p', text])
+    return raw_output.split("\n")[0].split("\t")[1:]
+
+if __name__ == '__main__':
+    assert MODEL_PATH.exists(), MODEL_PATH
+    unittest.main()
